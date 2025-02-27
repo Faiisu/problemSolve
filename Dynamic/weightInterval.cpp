86595a1df9e1af6a7722f44c0351294a35313102
Sukum Decha <sukumdecha@gmail.com>
2025-02-27 15:38:12 +0700

    add

--- Dynamic/weightInterval.cpp
+++ Dynamic/weightInterval.cpp
@@ -1,41 +1,27 @@
-#include<iostream>
-#include<map>
-#include<set>
+#include <iostream>
+#include <map>
+#include <set>
 
 using namespace std;
 int n;
 
-map<int, set< pair<int, pair<int,int> > > > times;
-//y, weight, x, i
-set< pair<int, pair<int ,pair< int, int> > > > totals;
-//weight, size, x, y
-int main(){
+map<int, set<pair<int, pair<int, int>>>> times;
+// y, weight, x, i
+set<pair<int, pair<int, pair<int, int>>>> totals;
+// weight, size, x, y
+int main()
+{
     cin >> n;
-    for(int i = 1 ; i <= n ; i++){
+    for (int i = 1; i <= n; i++)
+    {
         int x, y, w;
         cin >> x >> y >> w;
-        times[y].insert(make_pair(w, make_pair(x,i)));
+        times[y].insert(make_pair(w, make_pair(x, i)));
     }
 
-    for(auto ite : times){
+    for (auto ite : times)
+    {
         int y = ite.first;
-        for(auto rit = ite.second.rbegin(); rit!= ite.second.rend(); ++rit){
-            int w = rit->first;
-            int x = rit->second.first;
-            int i = rit->second.second;
-            
-            for(auto total = totals.rbegin(); total!=totals.rend(); ++total){
-                int t_w = total->first;
-                int t_size = total->second.first;
-                int t_x = total->second.second.first;
-                int t_y = total->second.second.second;
-
-                if(x >= t_x){
-                    
-                }
-            }
         }
-
-    }
     return 0;
 }
